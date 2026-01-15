e0fa92909e6c4fd38b63a4e132bca94bd158d265
HycJack <772403255@qq.com>
2026-01-15 19:24:25 +0800

    modify axiosInstance.ts

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -22,9 +22,19 @@ service.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token')
     if (token) {
-      config.headers = config.headers
       config.headers['Authorization'] = `Bearer ${token}`
     }
+    // 添加防缓存的请求头
+    config.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    config.headers['Pragma'] = 'no-cache'
+    config.headers['Expires'] = '0'
+    // 对于GET请求，添加时间戳参数以防止缓存
+    // if (config.method?.toLowerCase() === 'get') {
+    config.params = {
+      ...config.params,
+      _t: Date.now()
+    }
+    // }
     return config
   },
   (error: any) => {
